63395500b9870e2924238fdd96eeb0786652f5e4
Anton <golovanov.2000@mail.ru>
2023-11-23 20:32:11 +0300

    Editing

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,4 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('authorization',views.authorization, name = 'authorization')
 ]
--- main/views.py
+++ main/views.py
@@ -2,6 +2,3 @@ from django.shortcuts import render
 
 def index (request):
     return render(request, 'main/index.html')
-
-def authorization (request):
-    return render(request, 'main/authorization.html')
